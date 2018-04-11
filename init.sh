f10d36f2bc7a6d81d2f57efab56b9e74285837c3
蔡正海 <caizhenghai@gmail.com>
2018-04-11 11:56:41 +0800

    添加 bcmath 扩展

--- init.sh
+++ init.sh
@@ -26,7 +26,7 @@ sodu apt-get update > /dev/null
 ## 安装 nginx mysql php
 sudo apt-get update -y
 sudo apt-get install -y nginx
-sudo apt-get install -y php7.0 php7.0-fpm php7.0-cli php7.0-common php7.0-mbstring php7.0-gd php7.0-intl php7.0-xml php7.0-mysql php7.0-mcrypt php7.0-zip php7.0-curl
+sudo apt-get install -y php7.0 php7.0-fpm php7.0-cli php7.0-common php7.0-mbstring php7.0-gd php7.0-intl php7.0-xml php7.0-mysql php7.0-mcrypt php7.0-zip php7.0-curl php7.0-bcmath
 sudo apt-get install -y mysql-server
 
 #安装 autojump
